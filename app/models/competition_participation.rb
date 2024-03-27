98d50c074658a3713e432186011700d4a77906bc
Bergrebell <rrkuepper@gmail.com>
2024-03-27 09:58:33 +0100

    remove: unneccesary inverse_of

--- app/models/competition_participation.rb
+++ app/models/competition_participation.rb
@@ -1,4 +1,4 @@
 class CompetitionParticipation < ApplicationRecord
-  belongs_to :competition_edition, inverse_of: :competition_participations
+  belongs_to :competition_edition
   has_many :competition_incomes
 end
